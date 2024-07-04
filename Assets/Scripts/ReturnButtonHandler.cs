811b666f1d779bc73f01f116072c1eeb00a89ffc
Redas Goluenko <115810846+redasGoluenko@users.noreply.github.com>
2024-07-04 15:32:31 +0300

    Working On Glow Tab

--- Assets/Scripts/ReturnButtonHandler.cs
+++ Assets/Scripts/ReturnButtonHandler.cs
@@ -5,7 +5,14 @@ using UnityEngine.UI;
 
 public class ReturnButtonHandler : MonoBehaviour
 {
-    public Death death;
+    public Color newColor; // Color to change backgrounds to
+    private float fadeDuration = 0.125f; // Duration of the fade in seconds
+
+    public MoveDiagonally endless; // Reference to the MoveDiagonally script
+    public MoveDiagonally levels; // Reference to the MoveDiagonally script
+    public MoveDiagonally store; // Reference to the MoveDiagonally script
+    public MoveDiagonally exit; // Reference to the MoveDiagonally script
+    public Death death; // Reference to the Death script
     // Start is called before the first frame update
     void Start()
     {
@@ -19,14 +26,26 @@ public class ReturnButtonHandler : MonoBehaviour
     }
    
     public void OnButtonClick()
+    {   
+        // Start the coroutine to move exit diagonally left
+        StartCoroutine(exit.MoveLeftDiagonallyInitiallyCoroutine());
+        StartCoroutine(WaitAndMove(0.1f, store)); // Wait 1 second, then move store diagonally left
+        StartCoroutine(WaitAndMove(0.2f, levels)); // Wait 2 seconds, then move levels diagonally left
+        StartCoroutine(WaitAndMove(0.3f, endless)); // Wait 3 seconds, then move endless diagonally left       
+        death.CloseMenu(); // Close the menu       
+        StartCoroutine(LoadScene(1f)); // Wait 4 seconds, then load the scene "Endless"
+    }  
+    // Coroutine to wait for a specified time and then move the object diagonally left
+    IEnumerator WaitAndMove(float delay, MoveDiagonally moveDiagonally)
     {
-        Debug.Log("Button clicked");
-        death.CloseMenu();
-        StartCoroutine(LoadScene(1f));
+        yield return new WaitForSeconds(delay); // Wait for the specified time
+        StartCoroutine(moveDiagonally.MoveLeftDiagonallyInitiallyCoroutine()); // Start moving the object diagonally left
     }
+
+    // Coroutine to wait for a specified time and then load the scene "Endless"
     IEnumerator LoadScene(float delay)
     {
-       yield return new WaitForSeconds(delay);
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
-    }
+        yield return new WaitForSeconds(delay); // Wait for the specified time
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu"); // Load the scene "Endless"
+    } 
 }
